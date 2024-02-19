610796299382db00dcaf8d9d5b19331b6093469f
Cyril Declerck <cdeclerck@dalt.fr>
2024-02-19 15:49:42 +0100

    first commit

---
+++ components/ModalBadge.vue
@@ -0,0 +1,61 @@
+<!-- components/Modal.vue -->
+<template>
+    <div class="modal">
+      <div class="modal-content">
+        <span class="close" @click="closeModal">&times;</span>
+        <h2>Changer de propriétaire du badge</h2>
+        <form @submit.prevent="handleSubmit">
+          <label>Nouveau propriétaire :</label>
+          <input type="text" v-model="newOwner" required />
+          <button type="submit">Enregistrer</button>
+        </form>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  
+  
+  const newOwner = ref('')
+  
+  const handleSubmit = async () => {
+    closeModal()
+  }
+  
+  const closeModal = () => {
+    newOwner.value = ''
+    $emit('close')
+  }
+  </script>
+  
+  <style scoped>
+  /* Style spécifique au composant Modal */
+  .modal {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1000;
+  }
+  
+  .modal-content {
+    background-color: #333;
+    padding: 20px;
+    border-radius: 10px;
+  }
+  
+  .close {
+    position: absolute;
+    top: 10px;
+    right: 15px;
+    font-size: 20px;
+    cursor: pointer;
+  }
+  </style>
+  
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<!-- components/Header.vue -->
+<template>
+    <header>
+      <h1>Qui a le badge ?</h1>
+      <button @click="openModal">Changer de propriétaire</button>
+      <Modal v-if="isModalOpen" @close="isModalOpen = false" />
+    </header>
+  </template>
+  
+  <script setup>
+  import Modal from '~/components/ModalBadge.vue'
+  import { ref } from 'vue'
+  
+  const isModalOpen = ref(false)
+  
+  const openModal = async () => {
+    const [fileHandle] = await window.showOpenFilePicker();
+    const file = await fileHandle.getFile();
+    const contents = await file.text();
+    console.log(contents);
+    isModalOpen.value = true
+  }
+  </script>
+  
+  <style scoped>
+  /* Style spécifique au composant Header */
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+    background-color: #222;
+  }
+  
+  button {
+    background-color: #444;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  </style>
+  
